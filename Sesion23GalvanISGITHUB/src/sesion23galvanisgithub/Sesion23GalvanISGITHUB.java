6250bf5b0df4c706717d893418de07e293431ca9
ErnestoGL12 <ernesto.galo12@gmail.com>
2024-05-18 17:57:34 -0600

    Confirmacion Galvan 1
    OK

---
+++ Sesion23GalvanISGITHUB/src/sesion23galvanisgithub/Sesion23GalvanISGITHUB.java
@@ -0,0 +1,12 @@
+
+package sesion23galvanisgithub;
+
+
+public class Sesion23GalvanISGITHUB {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Prueba de GITHUB 1 Galvan");
+    }
+    
+}
