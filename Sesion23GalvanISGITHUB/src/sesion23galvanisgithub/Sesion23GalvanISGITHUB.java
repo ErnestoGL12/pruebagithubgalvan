8a0b7dc1de3869cdcf133d20b3c1686c55b960e4
ErnestoGL12 <ernesto.galo12@gmail.com>
2024-05-18 18:01:20 -0600

    Confirmacion 2 Galvan
    OK

--- Sesion23GalvanISGITHUB/src/sesion23galvanisgithub/Sesion23GalvanISGITHUB.java
+++ Sesion23GalvanISGITHUB/src/sesion23galvanisgithub/Sesion23GalvanISGITHUB.java
@@ -7,6 +7,10 @@ public class Sesion23GalvanISGITHUB {
    
     public static void main(String[] args) {
         System.out.println("Prueba de GITHUB 1 Galvan");
+        System.out.println("Prueba de GITHUB 2 Galvan");
+        System.out.println("Prueba de GITHUB 3 Galvan");
+        System.out.println("Prueba de GITHUB 4 Galvan");
+        System.out.println("Prueba de GITHUB 5 Galvan");
     }
     
 }
